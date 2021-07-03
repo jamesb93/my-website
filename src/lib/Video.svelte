192ee8b22030a1738637872ab557b1e9ee9a9560
James Bradbury <jamesbradbury93@gmail.com>
2021-07-03 16:44:02 +0100

    video element

---
+++ src/lib/Video.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let url = ''
+    const webm = `/vid/${url}.webm`
+    const mp4 = `/vid/${url}.mp4`
+</script>
+
+<video controls>
+    <source src={webm} type='video/webm'>
+    <source src={mp4} type='video/mp4'>
+    Sorry, your browser doesn't support embedded videos.
+</video>
+
+<style>
+    video { max-width: 100% }
+</style>
+
