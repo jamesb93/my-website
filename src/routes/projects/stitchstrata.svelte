d1e4d4d47d3783046aa03aba43a54f7ffb08c343
James Bradbury <jamesbradbury93@gmail.com>
2021-07-05 15:42:11 +0100

    fuck complex waveforms

--- src/routes/projects/stitchstrata.svelte
+++ src/routes/projects/stitchstrata.svelte
@@ -1,15 +1,12 @@
-<script>
-    import Waveform from "$lib/Waveform.svelte";
-</script>
-
 <h1>Stitch/Strata</h1>
 
-
-<Waveform 
-title=''
-file="/static/audio/StitchStrata.mp3"
-buffer="/static/waveforms/StitchStrata.dat"
-/>
+<audio controls>
+    <source 
+    type='audio/mp3'
+    src='https://user.fm/files/v2-b1db7989094f93e3c6c6e1b143ac35ac/StitchStrata.mp3'
+    >
+    <track kind="captions">
+</audio>
 
 <p>
     Stitch/Strata was composed using audio descriptors and Python scripts to carve out groups of samples from a corpus of vocal segments. These segments are drawn from <a href="https://sagepbbbt.com/daily-sketches/">Sage Pbbbt's daily sketches.</a> The first performance was at the 2019 Electric Spring Festival, where I was generously offered the opportunity to diffuse it over a 3D array of speakers.
@@ -18,3 +15,9 @@ buffer="/static/waveforms/StitchStrata.dat"
 <p>
     The code for this project was mostly made in an ad-hoc fashion and is largely untraceable. Nonetheless it is available <a href="https://github.com/jamesb93/Stitch-Strata">here</a> for the brave.
 </p>
+
+<style>
+    audio {
+        margin-top: 20px;
+    }
+</style>
