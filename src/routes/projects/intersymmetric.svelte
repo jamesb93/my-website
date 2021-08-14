d8be0eeb4d6bfb9eaab5f8d2629a9e4930bde956
James Bradbury <jamesbradbury93@gmail.com>
2021-08-14 12:33:44 +0100

    update projects

--- src/routes/projects/intersymmetric.svelte
+++ src/routes/projects/intersymmetric.svelte
@@ -5,13 +5,18 @@
 <h2>Intersymmetric</h2>
 
 <p>
-    Intersymmetric Sequencer 1 is an online multiplayer drum machine that I built with Mark Fell, Rian Treanor and Joe Gilmore. It uses a shared interface for manipulating pattern generation and sound synthesis. This enables groups of people to play together in real time. 
+    Intersymmetric Sequencer 1 is an online multiplayer drum machine that I built with <a target='_blank' href='http://www.markfell.com/wiki/'>Mark Fell</a>, <a target='_blank' href='http://riantreanor.com/'>Rian Treanor</a> and <a target='_blank' href='https://qubik.com/'>Joe Gilmore</a>. It uses a shared interface for manipulating pattern generation and sound synthesis. This enables groups of people to play together in real time. 
 </p>
 
 <p>
-    The ideas behind the project came into focus during the COVID lockdowns as an alternative to media streaming and other traditional approaches to web based delivery. It also draws upon longer term interests in interactive pattern generation in sound and music from electronic dance music traditions. 
+    The drum machine is built to work in the browser leveraging javascript and the web audio API. You can see a small snippet of it in action below. Alternatively, check out <a target='_blank' href='https://intersymmetric.xyz/seq-1/'>https://intersymmetric.xyz/</a> to try it for yourself.
 </p>
 
 <Video
 url='intersymmetric'
 />
+
+<p>
+    The code for the frontend is available <a target='_blank' href='https://github.com/jamesb93/intersymmetric'>here</a>.
+</p>
+
