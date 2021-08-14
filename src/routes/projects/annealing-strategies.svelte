8456cb712574f42179e74ed8894b4a4e65f84c2f
James Bradbury <jamesbradbury93@gmail.com>
2021-08-14 12:25:58 +0100

    update projects

--- src/routes/projects/annealing-strategies.svelte
+++ src/routes/projects/annealing-strategies.svelte
@@ -10,7 +10,8 @@ buffer="/static/waveforms/AnnealingStrategies.dat"
 
 <p>
     "Annealing Strategies" is a system, piece and patch that optimises and searches a chaotic parameter space using the process of simulated annealing. This algorithm mimics the physical mechanism of gradually cooling metal in order to remove internal stresses to strengthen it. The algorithm shares similarities with, and in a way is an improvement on the hill climbing algorithm, with the addition of a "temperature" coefficient that allows for local minima and maxima to be escaped from. Random decisions are allowed with respect to this coefficient, meaning the system is volatile and explorative at high temperatures and more conservative and exploitative at low temperatures. As the system is cooled the system hones in on an optimised solution. 
-
+</p>
+<p>
     The sound generating guts of the system are a digital emulation of Peter Blasser’s <a href='https://www.ciat-lonbarde.net)'>(Ciat-Lonbarde)</a> <a href='https://www.ciat-lonbarde.net/fyrall/index.html'>Fyrall</a>. A single input is used to control the 24 parameters of the synthesis engine by reading through a procedural basis function. You can see a short video of me playing with this interface below.
 </p>
 
@@ -20,9 +21,11 @@ buffer="/static/waveforms/AnnealingStrategies.dat"
     <track kind="captions">
 </video>
 
-The output of the Fourses synthesiser controlled through this mechniams is measured in decibels using an EBUR128 loudness descriptor. This forms a complex mapping between a single dial and the loudness descriptor with the complex coupled parameters between these. The simulated annealing algorithm searches for an input parameter that minimises this loudness descriptor, creating a pathway towards a quiet state.
-
-<style>
+<p>
+    The output of the Fourses synthesiser controlled through this mechniams is measured in decibels using an EBUR128 loudness descriptor. This forms a complex mapping between a single dial and the loudness descriptor with the complex coupled parameters between these. The simulated annealing algorithm searches for an input parameter that minimises this loudness descriptor, creating a pathway towards a quiet state.
+</p>
+    
+    <style>
     video {
         display: block;
         margin: 0 auto;
--- src/routes/projects/intersymmetric.svelte
+++ src/routes/projects/intersymmetric.svelte
@@ -1,1 +1,17 @@
-More coming...
+<script>
+    import Video from '$lib/Video.svelte';
+</script>
+
+<h2>Intersymmetric</h2>
+
+<p>
+    Intersymmetric Sequencer 1 is an online multiplayer drum machine that I built with Mark Fell, Rian Treanor and Joe Gilmore. It uses a shared interface for manipulating pattern generation and sound synthesis. This enables groups of people to play together in real time. 
+</p>
+
+<p>
+    The ideas behind the project came into focus during the COVID lockdowns as an alternative to media streaming and other traditional approaches to web based delivery. It also draws upon longer term interests in interactive pattern generation in sound and music from electronic dance music traditions. 
+</p>
+
+<Video
+url='intersymmetric'
+/>
