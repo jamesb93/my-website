a3508a7d8a4dbb94738c3743c3a5a3230e048b7d
James Bradbury <jamesbradbury93@gmail.com>
2021-09-19 15:14:06 +0100

    redirect to reacoma github

--- src/routes/reacoma.svelte
+++ src/routes/reacoma.svelte
@@ -1,4 +1,8 @@
 <script>
     import { goto } from '$app/navigation';
     import { onMount } from 'svelte';
+
+    onMount(async() => [
+        goto('https://www.github.com/reacoma')
+    ])
 </script>
