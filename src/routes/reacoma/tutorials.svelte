17453b901c695b5f4e17e625d1b53dc580e3477c
James Bradbury <me@jamesbradbury.net>
2021-10-31 12:24:35 +0000

    add some sneaky redirects for links

---
+++ src/routes/reacoma/tutorials.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+
+    onMount(async() => [
+        goto('https://www.youtube.com/watch?v=r3uHMXmlPRo&list=PLCQRw62RgghbsZgsA98lLkOwjBSs4yc9T')
+    ])
+</script>
