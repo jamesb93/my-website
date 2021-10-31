839b1e8cc27182399e95aaa6ebf5f94454e7f964
James Bradbury <me@jamesbradbury.net>
2021-10-31 12:45:27 +0000

    pimp up interferences page

--- src/routes/projects/interferences.svelte
+++ src/routes/projects/interferences.svelte
@@ -1,7 +1,15 @@
 <h1>Interferences</h1>
-<p>
-    Interferences is my coming in to contact with electromagnetic sounds, ever-present in the world around us but only detectable to our ears through induction microphones and equipment. Recording several hours of such sounds, I then deployed machine listening techniques to organise, structure and suggest to me ways in which these glacial, yet detailed and intricate sound worlds could merge and meld with each other. The two tracks in this album are the result of this process, and the dialogue between my intuitions and the computer’s interpreting of this rich sound world.
-</p>
+
+<div class='spiel'>
+    <p>Interferences is my coming in to contact with electromagnetic sounds, ever-present in the world around us but only detectable to our ears through induction microphones and equipment. Recording several hours of such sounds, I then deployed machine listening techniques to organise, structure and suggest to me ways in which these glacial, yet detailed and intricate sound worlds could merge and meld with each other. The two tracks in this album are the result of this process, and the dialogue between my intuitions and the computer’s interpreting of this rich sound world.</p>
+    <div class='artwork'>
+        <img src='/img/interferences.jpg' alt='Interferences album art' />
+        <div class='attribution'>
+            Album Artwork by <a target='_blank' href='https://caseyknapp.work/'>Casey Knapp</a>
+        </div>
+    </div>
+    
+</div>
 
 <div id='music-container'>
     <div id='item'>
@@ -9,8 +17,8 @@
         <audio controls>
             <source
             type='audio/mp3'
-             src='https://user.fm/files/v2-8c477bc22134b8f78453a4b917ad8768/G090G10564620B7Q.mp3'
-             >
+            src='https://user.fm/files/v2-8c477bc22134b8f78453a4b917ad8768/G090G10564620B7Q.mp3'
+            >
         </audio>
     </div>
 
@@ -30,16 +38,44 @@
 </p>
 
 <style>
-#music-container {
-    margin-top: 20px;
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-}
+    .spiel {
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        gap: 1em;
+    }
+    .spiel > p {
+        margin: 0;
+        padding: 0;
+        max-width: 40ch;
+    }
 
-#item {
-    display: flex;
-    flex-direction: column;
-    gap: 3px;
-}
+    .artwork {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .attribution {
+        font-style: italic;
+        font-size: 10px;
+        color: grey;
+    }
+    #music-container {
+        margin-top: 20px;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    #item {
+        display: flex;
+        flex-direction: column;
+        gap: 3px;
+    }
+
+    img {
+        height: 230px;
+        float:left;
+    }
 </style>
