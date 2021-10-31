4b0e6f4e31e3ce57bacc9185f2dd2db498385d97
James Bradbury <me@jamesbradbury.net>
2021-10-31 21:38:34 +0000

    add redirect for FTIS

---
+++ src/routes/ftis/index.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+
+    onMount(async() => [
+        goto('https://www.github.com/jamesb93/ftis')
+    ])
+</script>
