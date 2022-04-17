09de7006be6df5a696058386930fd6940564d179
James Bradbury <github@jamesbradbury.net>
2022-04-17 08:32:49 +0800

    update interferences page

--- src/routes/projects/interferences.svelte
+++ src/routes/projects/interferences.svelte
@@ -1,32 +1,30 @@
 <h1>Interferences</h1>
 
-<div class="spiel">
-	<p>
-		Interferences is my coming in to contact with electromagnetic sounds, ever-present in the world
-		around us but only detectable to our ears through induction microphones and equipment. Recording
-		several hours of such sounds, I then deployed machine listening techniques to organise,
-		structure and suggest to me ways in which these glacial, yet detailed and intricate sound worlds
-		could merge and meld with each other. The two tracks in this album are the result of this
-		process, and the dialogue between my intuitions and the computer’s interpreting of this rich
-		sound world.
-	</p>
-	<div class="artwork">
-		<img src="/img/interferences.jpg" alt="Interferences album art" />
-		<div class="attribution">
-			Album Artwork by <a target="_blank" href="https://caseyknapp.work/">Casey Knapp</a>
-		</div>
-	</div>
+<div class="container">
+	<iframe style="border: 0; width: 350px; height: 522px;" src="https://bandcamp.com/EmbeddedPlayer/album=2729925623/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/" seamless><a href="https://jamesbradbury.bandcamp.com/album/interferences">Interferences by James Bradbury</a></iframe>
 </div>
 
-<p>
-	Interferences is going to be released on <a href="https://tokinogake.bandcamp.com/"
-		>時の崖_tokinogake</a
-	> in March, 2022.
-</p>
 
 <p>
-	I talk about my aesthetic motivations and the code which assisted me in writing these two works in
-	significant detail in my thesis: <a
+	Interferences is my coming in to contact with electromagnetic sounds, ever-present in the world
+	around us but only detectable to our ears through induction microphones and equipment. Recording
+	several hours of such sounds, I then deployed machine listening techniques to organise,
+	structure and suggest to me ways in which these glacial, yet detailed and intricate sound worlds
+	could merge and meld with each other. The two tracks in this album are the result of this
+	process, and the dialogue between my intuitions and the computer’s interpreting of this rich
+	sound world.
+</p>
+<!-- <div class="artwork">
+	<img src="/img/interferences.jpg" alt="Interferences album art" />
+	<div class="attribution">
+		Album Artwork by <a target="_blank" href="https://caseyknapp.work/">Casey Knapp</a>
+	</div>
+</div> -->
+
+<p>Interferences is now released on <a href="https://tokinogake.bandcamp.com/">時の崖_tokinogake</a></p>
+
+<p>
+	I talk about my aesthetic motivations and the code which assisted me in writing these two works here: <a
 		target="blank"
 		href="https://harnessing.xyz/projects/interferences"
 		>https://harnessing.xyz/projects/interferences</a
@@ -34,43 +32,8 @@
 </p>
 
 <style>
-	.spiel {
-		display: flex;
-		flex-direction: row;
-		gap: 1em;
-	}
-	.spiel > p {
-		margin: 0;
-		padding: 0;
-		max-width: 40ch;
-	}
-
-	.artwork {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-
-	.attribution {
-		font-style: italic;
-		font-size: 10px;
-		color: grey;
-	}
-	#music-container {
-		margin-top: 20px;
-		display: flex;
-		flex-direction: column;
-		gap: 20px;
-	}
-
-	#item {
-		display: flex;
-		flex-direction: column;
-		gap: 3px;
-	}
-
-	img {
-		height: 230px;
-		float: left;
+	.container {
+		display: grid;
+		place-items: center;
 	}
 </style>
