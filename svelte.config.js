a6127e2aa2e7368d91020bf88caa913822631126
James Bradbury <github@jamesbradbury.net>
2022-06-14 12:27:35 +0000

    update for nginx

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,24 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 // /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		adapter: adapter(),
-// 		trailingSlash: 'always',
-// 		prerender: {
-// 			default: true,
-// 			enabled: true
-// 		}
-// 	}
-// };
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			edge: true
-		}),
+		adapter: adapter(),
+		trailingSlash: 'always',
+		prerender: {
+			default: true,
+			enabled: true
+		}
 	}
 };
+
+/** @type {import('@sveltejs/kit').Config} */
+// const config = {
+	// kit: {
+		// adapter: adapter({
+			// edge: true
+		// }),
+	// }
+// };
 
 export default config;
